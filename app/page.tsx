151e1a5afc512111ec74861c00045f7afd94bf99
Rushabh1134 <rushabhpatil557@gmail.com>
2024-10-02 11:44:25 +0530

    Add FlickeringGrid component and integrate it into the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,32 @@
-
 import FacebookFetchComponent from "@/components/FacebookFetch";
 import InstagramFetchComponent from "@/components/InstagramFetch";
 import OSINT from "@/components/OSINT";
 import SearchProfile from "@/components/SearchProfile";
+import FlickeringGrid from "@/components/ui/flickering-grid";
 import XFetchComponent from "@/components/XFetch";
 
 export default function Home() {
   return (
-    <div>   
-   <div className="text-5xl font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-600 via-blue-800 to-black drop-shadow-lg pt-10">
-  CyberNetra
-</div>
-    <SearchProfile></SearchProfile>
-        
+    <div className="relative">
+      <FlickeringGrid className="-z-10 absolute inset-0" 
+        squareSize={4}
+        gridGap={8}
+        color="#60A5FA"
+        maxOpacity={0.3}
+        flickerChance={0.1} />
+      <div className="text-5xl font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-600 via-blue-800 to-black drop-shadow-lg pt-10">
+        CyberNetra
+      </div>
+      <SearchProfile></SearchProfile>
+
       <div className="grid grid-cols-3 min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        {/* <Test {data}/> */}    
+        {/* <Test {data}/> */}
 
         <InstagramFetchComponent></InstagramFetchComponent>
         <FacebookFetchComponent></FacebookFetchComponent>
         <XFetchComponent></XFetchComponent>
       </div>
-      <OSINT/>
+      <OSINT />
     </div>
   );
-}     
+}
